be2069203a170e009043d1c9c22aa4ec83a49260
DinoMatheow <matius152014@gmail.com>
2026-04-29 13:49:34 -0500

    feat: Implementacion de api, y pruebas de servicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-export const CivaApp = () => {
+import { getListBus } from "./bus/api/get-list-bus";
+
+export const CivaApp =  () => {
+
+  // await getListBus();
+
   return(
     <div>
       <h1 className="text-5xl font-bold">
       Hola mundo
       </h1>
       <button className="btn btn-primary">Buscar</button>
-      
       </div>
   )
 }
---
+++ src/bus/interfaces/civa.response.ts
@@ -0,0 +1,9 @@
+export interface CivaResponse {
+    id:         number;
+    numberBus:  string;
+    plate:      string;
+    createdAt:  Date;
+    attributes: string;
+    status:     string;
+    marcaBus:   string;
+}
