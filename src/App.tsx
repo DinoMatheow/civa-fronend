81349c49170e20aec84457d3faeb820c4ee7dc3b
DinoMatheow <matius152014@gmail.com>
2026-04-29 03:44:52 -0500

    feat: Configuración inicial del proyecto con React, TypeScript, Tailwind CSS v3 y DaisyUI v4

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export const CivaApp = () => {
+  return(
+    <div>
+      <h1 className="text-5xl font-bold">
+      Hola mundo
+      </h1>
+      <button className="btn btn-primary">Buscar</button>
+      
+      </div>
+  )
+}
