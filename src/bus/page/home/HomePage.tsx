fee2e55caae57a47d41c8a13c9ddc587179ee683
DinoMatheow <matius152014@gmail.com>
2026-04-29 19:38:29 -0500

    feat: implementacion de api en home

--- src/bus/page/home/HomePage.tsx
+++ src/bus/page/home/HomePage.tsx
@@ -1,4 +1,22 @@
-export const HomePage = () => {
+import { use, useEffect, useState } from "react"
+import { getListBus } from "../../api/get-list-bus";
+
+export const HomePage = async () => {
+
+  const [bus, setBusList] = useState<string[]>([]);
+
+  useEffect(()=> {
+    const fetchBuses = async ()=> {
+      const data = await getListBus();
+      setBusList(data);
+
+    };
+    fetchBuses();
+  }, [])
+
+  const busList = await getListBus();
+  console.log(busList);
+
     return (
     <div>
       <h1 className="text-5xl font-bold">
