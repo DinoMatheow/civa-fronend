0ab878e42587a0765d0e422a9087d23e31a980e2
Matheow2 <matius152014@gmail.com>
2026-05-18 17:03:46 -0500

    feat: Implementacion de carga perezosa en SearchPage

--- src/router/app.route.tsx
+++ src/router/app.route.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router";
 import { HomePage } from "../bus/page/home/HomePage";
-import { SearchPage } from "../bus/page/search/SearchPage";
+// import { SearchPage } from "../bus/page/search/SearchPage";
 import { BusLayouts } from "../bus/layouts/BusLayouts";
+import { lazy } from "react";
 
+const SearchPage = lazy(()=> import("../bus/page/search/SearchPage"));
 
 export const router = createBrowserRouter([
   {
